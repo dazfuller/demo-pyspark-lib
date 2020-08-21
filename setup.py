126f90e0ffedc305dd03449efc9043968e0ae831
Darren Fuller <daz.fuller@gmail.com>
2020-08-21 15:03:31 +0100

    add setup to create wheel

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from setuptools import setup
+
+setup(
+    name="dazspark",
+    version="0.0.1",
+    description="A demo PySpark library",
+    url="https://github.com/dazfuller/demo-pyspark-lib",
+    author="dazfuller",
+    author_email="daz.fuller@gmail.com",
+    packages=[
+        "dazspark"
+    ],
+    install_requires=[
+        "pyspark==3.0.0",
+        "pandas==1.1.0",
+        "pyarrow==1.0.0"
+    ],
+    zip_safe=False
+)
