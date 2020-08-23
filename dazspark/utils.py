9a0cc3e5fa9cbf818e53dd6a59087e5df9f6458c
Darren Fuller <daz.fuller@gmail.com>
2020-08-23 13:37:54 +0100

    implement remaining transformations

--- dazspark/utils.py
+++ dazspark/utils.py
@@ -1,5 +1,8 @@
 import re
+from functools import reduce
 from typing import Optional
+
+from pyspark.sql import DataFrame
 
 
 def replace_invalid_chars(col_name: str, replace_char: Optional[str] = "_") -> str:
@@ -14,7 +17,7 @@ def replace_invalid_chars(col_name: str, replace_char: Optional[str] = "_") -> s
     Returns:
         str: [description]
     """
-    return re.sub("[ ,;{}()\n\t=\\/.]", "_", col_name)
+    return re.sub("_+", replace_char, re.sub("[ ,;{}()\n\t=\\/.]", replace_char, col_name))
 
 
 def standardise_name(col_name: str) -> str:
@@ -30,3 +33,15 @@ def standardise_name(col_name: str) -> str:
         str: A standardised column name
     """
     return re.sub("_+", "_", "_".join([part.capitalize() for part in re.split("[ _]", replace_invalid_chars(col_name))]).strip("_"))
+
+
+def standardise_data_frame(df: DataFrame) -> DataFrame:
+    """Standardises the column names of a Spark DataFrame
+
+    Args:
+        df (DataFrame): The DataFrame to standardise the column names of
+
+    Returns:
+        DataFrame: DataFrame with columns renamed and standardised
+    """
+    return reduce(lambda acc, col: acc.withColumnRenamed(col, standardise_name(col)), df.columns, df)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,9 @@
-import unittest
+from typing import List
 
+from pyspark.sql.dataframe import DataFrame
+from tests.PySparkTestCase import PySparkTestCase
+import unittest
+from pyspark.sql import Row
 import dazspark.utils as u
 
 
@@ -8,3 +12,55 @@ class TestUtilsColumnNameCleaning(unittest.TestCase):
         input: str = "Example Column Name"
         expected: str = "Example_Column_Name"
         self.assertEqual(expected, u.replace_invalid_chars(input))
+
+    def test_removal_of_special_characters(self):
+        input: str = "Life; Is {beautiful}\nif you let=(it).be\tso"
+        expected: str = "Life_Is_beautiful_if_you_let_it_be_so"
+        self.assertEqual(expected, u.replace_invalid_chars(input))
+
+    def test_no_changes_on_valid_string(self):
+        input: str = "example_name"
+        expected: str = "example_name"
+        self.assertEqual(expected, u.replace_invalid_chars(input))
+
+    def test_multiple_spaces_collapse_to_single(self):
+        input: str = "example . name"
+        expected: str = "example_name"
+        self.assertEqual(expected, u.replace_invalid_chars(input))
+
+    def test_alternative_replace_char(self):
+        input: str = "example_name"
+        expected: str = "example~name"
+        self.assertEqual(expected, u.replace_invalid_chars(input, replace_char="~"))
+
+
+class TestUtilsColumnNameStandardisation(unittest.TestCase):
+    def test_standardising_simple_column(self):
+        input: str = "EXAMPLE COLUMN NAME"
+        expected: str = "Example_Column_Name"
+        self.assertEqual(expected, u.standardise_name(input))
+
+    def test_multiple_special_characters(self):
+        input: str = "Life; Is {beautiful}\nif you let=(it).be\tso"
+        expected: str = "Life_Is_Beautiful_If_You_Let_It_Be_So"
+        self.assertEqual(expected, u.standardise_name(input))
+
+    def test_leading_and_trailing_seperator_chars(self):
+        input: str = "{weird column}"
+        expected: str = "Weird_Column"
+        self.assertEqual(expected, u.standardise_name(input))
+
+
+class TestUtilsColumnCleaningWithPySpark(PySparkTestCase):
+    def test_dataframe_column_cleaning(self):
+        expected_cols: List[str] = ["Item_Id", "Item_Name", "Value_Usd"]
+        data_row: Row = Row("item.id", "item.name", "value (USD)")
+        data = [
+            data_row(1, "item 1", 123.45),
+            data_row(2, "item 2", 543.21)
+        ]
+
+        df: DataFrame = self.spark.createDataFrame(data)
+        standardised_df: DataFrame = u.standardise_data_frame(df)
+
+        self.assertListEqual(expected_cols, standardised_df.columns)
