6266f8f3e37a95e8cb8319c4b3201c55e9d7d4be
Darren Fuller <daz.fuller@gmail.com>
2020-08-24 11:35:35 +0100

    switch to major.minor format

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dazspark",
-    version="0.0.1",
+    version="0.1",
     description="A demo PySpark library",
     url="https://github.com/dazfuller/demo-pyspark-lib",
     author="dazfuller",
