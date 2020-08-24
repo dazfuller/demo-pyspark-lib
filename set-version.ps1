9bfa26af6e2934ac51f689a31533c00a1921a084
Darren Fuller <daz.fuller@gmail.com>
2020-08-24 14:53:14 +0100

    Dazfuller/change version schema (#10)
    * switch to major.minor format
    
    * modify to handle dev and final release
    
    * update pipeline to increment final version
    
    * temporarily remove prod condition check
    
    * update to handle prod version in build section
    
    * testing out prod release process
    
    * tweaks to pipeline for prod releases
    
    * fix merge issues

--- set-version.ps1
+++ set-version.ps1
@@ -1,9 +1,32 @@
 param (
-    [Parameter(Mandatory=$true)] [string] $BuildId
+    [Parameter(Mandatory=$false)] [switch] $IsDevRelease,
+    [Parameter(Mandatory=$false)] [string] $BuildId
 )
 
-$updatedSetup = (Get-Content ./setup.py) -replace "0.0.1", "0.0.$BuildId"
-$updatedModule = (Get-Content ./dazspark/__init__.py) -replace "0.0.1", "0.0.$BuildId"
+if ($IsDevRelease -and $BuildId -eq $null) {
+    throw "A build id number must be provided for development releases"
+}
+
+$versionMatch = Get-Content ./setup.py | Select-String -Pattern "version=`"(.+)`""
+
+if ($null -eq $versionMatch) {
+    throw "Unable to find version information"
+} else {
+    $currentVersion = $versionMatch.Matches.Groups[1].Value
+}
+
+$nextVersion = $currentVersion
+
+if ($IsDevRelease) {
+    $nextVersion = "$currentVersion.dev$BuildId"
+} else {
+    $versionParts = $currentVersion.Split(".")
+    $versionParts[-1] = ([int]$versionParts[-1]) + 1
+    $nextVersion = $versionParts -join "."
+}
+
+$updatedSetup = (Get-Content ./setup.py) -replace $currentVersion, $nextVersion
+$updatedModule = (Get-Content ./dazspark/__init__.py) -replace $currentVersion, $nextVersion
 
 $updatedSetup | Out-File ./setup.py
 $updatedModule | Out-File ./dazspark/__init__.py
